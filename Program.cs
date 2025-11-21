4130b3907e8fa242933663f533a7a37e1e9fe02b
Krasko08 <204331648+Krasko08@users.noreply.github.com>
2025-11-21 19:40:02 +0200

    Refactor curve classes and update variable names

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace GeometryLab
 {
-    // Інтерфейс для кривих
     public interface ICurve
     {
         void SetCoefficients();
@@ -10,115 +9,109 @@ namespace GeometryLab
         bool ContainsPoint(double x, double y);
     }
 
-    // Абстрактний клас
     public abstract class CurveBase : ICurve
     {
         public abstract void SetCoefficients();
         public abstract void PrintCoefficients();
         public abstract bool ContainsPoint(double x, double y);
 
-        ~CurveBase()
-        {
-            Console.WriteLine("Destructor of CurveBase called");
-        }
+        ~CurveBase() { }
     }
 
-    // Клас Еліпс
     public class Ellipse : CurveBase
     {
-        private double a;
-        private double b;
+        private double _a;
+        private double _b;
+
+        public double A
+        {
+            get => _a;
+            set { if (value > 0) _a = value; }
+        }
+
+        public double B
+        {
+            get => _b;
+            set { if (value > 0) _b = value; }
+        }
 
         public Ellipse() { }
 
         public Ellipse(double a, double b)
         {
-            this.a = a;
-            this.b = b;
+            A = a;
+            B = b;
         }
 
         public override void SetCoefficients()
         {
             Console.Write("Enter a: ");
-            a = Convert.ToDouble(Console.ReadLine());
+            A = double.Parse(Console.ReadLine());
             Console.Write("Enter b: ");
-            b = Convert.ToDouble(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
         }
 
         public override void PrintCoefficients()
         {
-            Console.WriteLine($"Ellipse: x^2/{a}^2 + y^2/{b}^2 = 1");
+            Console.WriteLine($"Ellipse: x^2/{A}^2 + y^2/{B}^2 = 1");
         }
 
         public override bool ContainsPoint(double x, double y)
         {
-            return (x * x) / (a * a) + (y * y) / (b * b) <= 1;
+            double value = (x * x) / (A * A) + (y * y) / (B * B);
+            return value <= 1.000000001;
         }
 
-        ~Ellipse()
-        {
-            Console.WriteLine("Destructor of Ellipse called");
-        }
+        ~Ellipse() { }
     }
 
-    // Квадратична крива другого порядку
-    public class SecondOrderCurve : CurveBase
+    public class QuadraticCurve : CurveBase
     {
-        private double a11, a12, a22, b1, b2, c;
-
-        public SecondOrderCurve() { }
+        private double _a11, _a12, _a22, _b1, _b2, _c;
 
         public override void SetCoefficients()
         {
-            Console.Write("a11: "); a11 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("a12: "); a12 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("a22: "); a22 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("b1: "); b1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("b2: "); b2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("c: "); c = Convert.ToDouble(Console.ReadLine());
+            Console.Write("a11: "); _a11 = double.Parse(Console.ReadLine());
+            Console.Write("a12: "); _a12 = double.Parse(Console.ReadLine());
+            Console.Write("a22: "); _a22 = double.Parse(Console.ReadLine());
+            Console.Write("b1: "); _b1 = double.Parse(Console.ReadLine());
+            Console.Write("b2: "); _b2 = double.Parse(Console.ReadLine());
+            Console.Write("c: "); _c = double.Parse(Console.ReadLine());
         }
 
         public override void PrintCoefficients()
         {
-            Console.WriteLine($"{a11}x^2 + 2{a12}xy + {a22}y^2 + {b1}x + {b2}y + {c} = 0");
+            Console.WriteLine($"{_a11}x^2 + 2{_a12}xy + {_a22}y^2 + {_b1}x + {_b2}y + {_c} = 0");
         }
 
         public override bool ContainsPoint(double x, double y)
         {
-            double val = a11 * x * x + 2 * a12 * x * y + a22 * y * y + b1 * x + b2 * y + c;
-            return Math.Abs(val) < 1e-6;
+            double val = _a11 * x * x + 2 * _a12 * x * y + _a22 * y * y + _b1 * x + _b2 * y + _c;
+            return Math.Abs(val) < 1e-9;
         }
 
-        ~SecondOrderCurve()
-        {
-            Console.WriteLine("Destructor of SecondOrderCurve called");
-        }
+        ~QuadraticCurve() { }
     }
 
-    // Демонстрація роботи
     class Program
     {
         static void Main()
         {
-            Ellipse ellipse = new Ellipse();
+            ICurve ellipse = new Ellipse();
             ellipse.SetCoefficients();
             ellipse.PrintCoefficients();
 
-            Console.Write("Enter test point X: ");
-            double x = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter test point Y: ");
-            double y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter X: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Enter Y: ");
+            double y = double.Parse(Console.ReadLine());
 
-            Console.WriteLine(ellipse.ContainsPoint(x, y)
-                ? "Point belongs to ellipse"
-                : "Point does NOT belong to ellipse");
+            Console.WriteLine(ellipse.ContainsPoint(x, y) ? "Point is inside ellipse" : "Point is outside ellipse");
 
-            SecondOrderCurve curve = new SecondOrderCurve();
+            ICurve curve = new QuadraticCurve();
             curve.SetCoefficients();
             curve.PrintCoefficients();
-            Console.WriteLine(curve.ContainsPoint(x, y)
-                ? "Point satisfies second‑order curve"
-                : "Point does NOT satisfy curve equation");
+            Console.WriteLine(curve.ContainsPoint(x, y) ? "Point satisfies quadratic curve" : "Point does NOT satisfy curve");
         }
     }
 }
